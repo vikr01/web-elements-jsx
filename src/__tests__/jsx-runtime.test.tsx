8275ea8f9d461e231d09f8486fe62ab5be860429
vikr01 <vik120@icloud.com>
2025-06-02 17:13:55 +0530

    support custom document fragment classes

--- src/__tests__/jsx-runtime.test.tsx
+++ src/__tests__/jsx-runtime.test.tsx
@@ -1,14 +1,22 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Fragment } from "../jsx-runtime";
 
 describe("jsx-runtime", () => {
-	test("properly converts the function call to proper elements", () => {
-		expect(<div/>).toBeInstanceOf(HTMLDivElement);
-		expect(<a/>).toBeInstanceOf(HTMLAnchorElement);
-	});
+  it("properly converts the function call to proper elements", () => {
+    expect(<div />).toBeInstanceOf(HTMLDivElement);
+    expect(<a />).toBeInstanceOf(HTMLAnchorElement);
+  });
 
-	test("can handle document fragments", () => {
-		expect(<></>).toBeInstanceOf(DocumentFragment);
-		expect(<Fragment></Fragment>).toBeInstanceOf(DocumentFragment);
-	});
+  it("can handle document fragments", () => {
+    expect(<></>).toBeInstanceOf(DocumentFragment);
+    expect(<Fragment></Fragment>).toBeInstanceOf(DocumentFragment);
+  });
+
+  it("can handle custom document fragments", () => {
+    class MyCustomDocumentFragment extends DocumentFragment {}
+
+    expect(
+      <MyCustomDocumentFragment></MyCustomDocumentFragment>,
+    ).toBeInstanceOf(MyCustomDocumentFragment);
+  });
 });
--- src/jsx-runtime-builder.ts
+++ src/jsx-runtime-builder.ts
@@ -4,69 +4,122 @@
  * https://babeljs.io/docs/babel-plugin-transform-react-jsx#importsource
  **/
 
-type CustomTagElementNameMapSuperType = {[key: string]: Element};
+type CustomTagElementNameMapSuperType = { [key: string]: Element };
 
-type TagClassMapMerged<TStrToClass extends CustomTagElementNameMapSuperType> = Omit<HTMLElementTagNameMap, keyof TStrToClass> & TStrToClass;
-type Tag<T extends CustomTagElementNameMapSuperType> = keyof TagClassMapMerged<T>;
+type TagClassMapMerged<TStrToClass extends CustomTagElementNameMapSuperType> =
+  Omit<HTMLElementTagNameMap, keyof TStrToClass> & TStrToClass;
+type Tag<T extends CustomTagElementNameMapSuperType> =
+  keyof TagClassMapMerged<T>;
 
-const fragmentSymbol: unique symbol = Symbol('fragment');
+const fragmentSymbol: unique symbol = Symbol("fragment");
 
-export type JsxFn<TCustomTagElementNameMap extends CustomTagElementNameMapSuperType> = {
-	<TFrag extends DocumentFragment = DocumentFragment>(tag: typeof fragmentSymbol | TFrag, props: Readonly<{children?: undefined | null | string | Element}> | undefined | never): TFrag;
-	<TTag extends Tag<TCustomTagElementNameMap>>(tag: TTag, props: Readonly<Record<string, string>>): TagClassMapMerged<TCustomTagElementNameMap>[TTag]
+type FragmentProps<T extends DocumentFragment> = Readonly<{
+  children?: undefined | null | Parameters<T["append"]>;
+}>;
+type AllowedFragmentTypes<T extends DocumentFragment> =
+  | typeof fragmentSymbol
+  | { new (): T };
+
+export type JsxFn<
+  TCustomTagElementNameMap extends CustomTagElementNameMapSuperType,
+> = {
+  <TFrag extends DocumentFragment = DocumentFragment>(
+    tag: AllowedFragmentTypes<TFrag>,
+    props: FragmentProps<TFrag>,
+  ): TFrag;
+  <TTag extends Tag<TCustomTagElementNameMap>>(
+    tag: TTag,
+    props: Readonly<Record<string, string>>,
+  ): TagClassMapMerged<TCustomTagElementNameMap>[TTag];
 };
 
-export function jsx<
-		T extends CustomTagElementNameMapSuperType,
-		TTag extends Tag<T>
-	>(tag: TTag | typeof fragmentSymbol, props: Readonly<Record<string, string>>): TagClassMapMerged<T>[TTag] | DocumentFragment {
-		if (tag === fragmentSymbol) {
-			const fragmentContainer = document.createDocumentFragment();
-			const children = props?.children ?? null;
-			if (children !== null) {
+function jsxFragment<TFrag extends DocumentFragment = DocumentFragment>(
+  tag: AllowedFragmentTypes<TFrag>,
+  props: FragmentProps<TFrag>,
+): TFrag {
+  let fragmentContainer: TFrag;
+  if (tag === fragmentSymbol) {
+    fragmentContainer = document.createDocumentFragment() as TFrag;
+  } else if (
+    typeof tag === "function" &&
+    tag.prototype instanceof DocumentFragment
+  ) {
+    fragmentContainer = new tag();
+  } else {
+    throw new Error(`Invalid JSX fragment received: ${tag?.name ?? tag}`);
+  }
 
-			}
-			return fragmentContainer;
-		}
+  if (props?.children != null) {
+    fragmentContainer.append.call(props.children);
+  }
 
-	const element = document.createElement((tag as any)) as TagClassMapMerged<T>[typeof tag];
-
-	for (const key in props) {
-		const v = props[key];
-
-		if (key === "children") {
-			element.append(v);
-			continue;
-		}
-
-		element.setAttribute(key, v);
-	}
-
-	return element;
+  return fragmentContainer;
 }
 
-type ForcedFragmentTypeByTypescript = {new (...args: any[]): DocumentFragment};
+export function jsx<
+  T extends CustomTagElementNameMapSuperType,
+  TTag extends Tag<T>,
+  TFrag extends DocumentFragment = DocumentFragment,
+>(
+  tag: TTag | AllowedFragmentTypes<TFrag>,
+  props: Readonly<Record<string, string>>,
+): TagClassMapMerged<T>[TTag] | TFrag {
+  if (
+    tag === fragmentSymbol ||
+    (typeof tag === "function" && tag.prototype instanceof DocumentFragment)
+  ) {
+    return jsxFragment<TFrag>(tag as AllowedFragmentTypes<TFrag>, props);
+  }
+
+  if (typeof tag === "function") {
+    throw new Error("This is not supported yet.");
+  }
+
+  const element = document.createElement(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    tag as any,
+  ) as TagClassMapMerged<T>[typeof tag];
+
+  for (const key in props) {
+    const v = props[key];
+
+    if (key === "children") {
+      element.append(v);
+      continue;
+    }
+
+    element.setAttribute(key, v);
+  }
+
+  return element;
+}
+
+type ForcedFragmentTypeByTypescript = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  new (...args: any[]): DocumentFragment;
+};
 
 export default function build<
-	T extends CustomTagElementNameMapSuperType,
-	>(): Readonly<{
-		jsx: JsxFn<T>,
-		jsxs: JsxFn<T>,
-		jsxDEV: JsxFn<T>,
-		Fragment: ForcedFragmentTypeByTypescript,
-	}> {
-	const _jsx = jsx as JsxFn<T>;
+  T extends CustomTagElementNameMapSuperType,
+>(): Readonly<{
+  jsx: JsxFn<T>;
+  jsxs: JsxFn<T>;
+  jsxDEV: JsxFn<T>;
+  Fragment: ForcedFragmentTypeByTypescript;
+}> {
+  const _jsx = jsx as JsxFn<T>;
 
-	return {
-		jsx: _jsx,
-		jsxs: _jsx,
-		jsxDEV: _jsx,
-		Fragment: fragmentSymbol as unknown as ForcedFragmentTypeByTypescript,
-	};
+  return {
+    jsx: _jsx,
+    jsxs: _jsx,
+    jsxDEV: _jsx,
+    Fragment: fragmentSymbol as unknown as ForcedFragmentTypeByTypescript,
+  };
 }
 
 export type IntrinsicElements<T extends CustomTagElementNameMapSuperType> = {
-	[K in Tag<T>]: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [K in Tag<T>]: any;
 };
 
 type El = Element | DocumentFragment;
